943255d9346fdb3357e000d966a02c75e8bc2fd3
Mike Kabbabe <83938405+33Mic@users.noreply.github.com>
2024-10-24 17:32:38 -0600

    Added some new files

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,23 @@ import java.util.ArrayList;
 
 public class CustomListTest {
 
+    public CustomList MockCityList() {
+        CustomList list = new CustomList(null, new ArrayList<>());
+        return list;
 
+    }
+
+    @Test
+    public void addCityTest() {
+        CustomList list = MockCityList();
+        int expectedCountBeforeAdd = 0;
+        assertEquals(expectedCountBeforeAdd, list.getCount(), "Count should be 0 before adding cities");
+
+        City cityToAdd = new City("Edmonton", "Alberta");
+        list.add(cityToAdd);
+
+        // Check that the count has been incremented after adding a city
+        int expectedCountAfterAdd = 1;
+        assertEquals(expectedCountAfterAdd, list.getCount(), "Count should be 1 after adding a city");
+    }
 }
